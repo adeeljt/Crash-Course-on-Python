0609d3000d5f272ad9238b37b782bb10e99d1d61
unknown <adeelarif.jt@outlook.com>
2023-08-30 10:24:48 +0500

    second addition

---
+++ 2 second program week 1.py
@@ -0,0 +1,4 @@
+# concatination in print function
+name = "Adeel"
+print("Hello " + name)
+print ("Hello " + name + "!")
---
+++ 3 as calculator.py
@@ -0,0 +1,13 @@
+# as calculator
+
+print(4 +5)
+
+print(9*7)
+
+print(9/3)
+
+print(1-3)
+
+print (1/3)
+
+print ((2+6)*12)
